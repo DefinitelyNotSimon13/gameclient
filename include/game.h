943e5cd92228c5f8e01e1480e31f7abbe53525b8
Simon <simon21.blum@gmail.com>
2025-01-08 15:07:54 +0100

    Start working on Multithreading
    MainThread: MainGameLoop
    Thread 2: Receiving Informations from Server (UDP)
    Thread 3: Receiving Informations from Server (TCP)
    
    Something like that
    Communcation between threads mainly over GameState mutex - main
    communication is PlayerPositions / active players

--- include/game.h
+++ include/game.h
@@ -7,6 +7,11 @@
 
 typedef PacketV0 OtherPlayer;
 
+typedef struct {
+    int active_players;
+    OtherPlayer players[];
+} GameState;
+
 void init_game(int screen_width, int screen_height);
 
 void run_game_loop(Player *player, Camera *camera, connection_t *conn);
